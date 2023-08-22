4f6cca3c7931afd477b996a65a57fa525554dc94
zunema <yxxa.abc@gmail.com>
2023-08-22 23:57:35 +0900

    Initial Commit

---
+++ HealthKitApi/HealthKitApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitApiApp.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
