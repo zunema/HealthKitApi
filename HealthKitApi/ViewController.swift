6f108d018a1c8f6336188b2e7b79091a33a01785
zunema <yxxa.abc@gmail.com>
2023-08-25 00:06:39 +0900

    コレクション操作

---
+++ HealthKitApi/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/08/24.
+//
+
+import SwiftUI
+import FirebaseFirestore
+
+class ViewController: UIViewController {
+    
+    var db: Firestore!
+    
+    private func setDocument() {
+//        db.collection("users").document("LA").setData([
+//                    "name": "Los Angeles",
+//                    "state": "CA",
+//                    "country": "USA"
+//                ]) { err in
+//                    if let err = err {
+//                        print("Error writing document: \(err)")
+//                    } else {
+//                        print("Document successfully written!")
+//                    }
+//                }
+    }
+}
