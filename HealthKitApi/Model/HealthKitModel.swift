ae6b2b17210c116af5a43faca9469c2052d84174
zunema <yxxa.abc@gmail.com>
2023-09-21 23:24:06 +0900

    睡眠データを格納する処理を実装

--- HealthKitApi/Model/HealthKitModel.swift
+++ HealthKitApi/Model/HealthKitModel.swift
@@ -14,6 +14,7 @@ class HealthKitModel: ObservableObject, Identifiable {
     var permissionMessage: String = ""
     var permissionFlg: Bool = false
     var textConfirm: String = "確認OK。"
+    var dataSource:[SleepItem] = []
     
     // 消費エネルギー、サイクリング、ウォーキング、ランニングの距離と心拍数の共有と読み出しに関する許可要求設定
     let allTypes = Set([HKObjectType.workoutType(),
@@ -70,29 +71,28 @@ class HealthKitModel: ObservableObject, Identifiable {
         }
     }
     
-    func getPermissionConfirmation() {
-    }
-    
     func getSleepAnalysis(fallingAsleepTime: Date, wakeUpTime: Date)  {
-            let query = HKSampleQuery(sampleType: HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!,
-                                      predicate: HKQuery.predicateForSamples(withStart: fallingAsleepTime, end: wakeUpTime, options: []),
-                                      limit: HKObjectQueryNoLimit,
-                                      sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)]){ (query, results, error) in
-                
-                guard error == nil else { print("睡眠データを取得失敗"); return }
-                // ここで取得した睡眠データを扱える形に整形する
-                if let results = results as? [HKCategorySample] {
-                    print("取得成功!!")
-//                    let reversedResults = results.reversed()
-//                    DispatchQueue.main.async {
-//                        reversedResults.map { data in
-//                            print(data.value.description)
-//                            // 睡眠データを使った処理を書く
-//                        }
-//                    }
+        let query = HKSampleQuery(sampleType: HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!,
+                                  predicate: HKQuery.predicateForSamples(withStart: fallingAsleepTime, end: wakeUpTime, options: []),
+                                  limit: HKObjectQueryNoLimit,
+                                  sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)]){ (query, results, error) in
+            
+            guard error == nil else { print("睡眠データを取得失敗"); return }
+            // ここで取得した睡眠データを扱える形に整形する
+            if let results = results as? [HKCategorySample] {
+                print("取得成功!!")
+                for item in results {
+                    let listItem = SleepItem(
+                        id: item.uuid.uuidString,
+                        startDateTime: item.startDate,
+                        endDateTime: item.endDate,
+                        sleepStatus: item.value.description
+                    )
+                    self.dataSource.append(listItem)
                 }
             }
-            healthStore.execute(query)
         }
-    
+        healthStore.execute(query)
+        print(type(of: self.dataSource))
+    }
 }
