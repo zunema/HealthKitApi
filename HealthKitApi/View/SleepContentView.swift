8888484195ae6f369ae8077a235d01684ebed8bc
zunema <yxxa.abc@gmail.com>
2023-09-30 18:46:15 +0900

    データ表示処理の修正

--- HealthKitApi/View/SleepContentView.swift
+++ HealthKitApi/View/SleepContentView.swift
@@ -13,51 +13,56 @@ struct SleepContentView: View {
     @Binding var fallingAsleepTime: Date
     @Binding var wakeUpTime: Date
         
-    @State var fmt: DateFormatter = {
+    @State var fmtDate: DateFormatter = {
         var fmt = DateFormatter()
         fmt.timeZone = TimeZone(identifier: "Asia/Tokyo")
         fmt.locale = Locale(identifier: "ja_JP")
-        fmt.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        fmt.dateFormat = "yyyy/MM/dd"
+        return fmt
+    }()
+    
+    @State var fmtTime: DateFormatter = {
+        var fmt = DateFormatter()
+        fmt.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        fmt.locale = Locale(identifier: "ja_JP")
+        fmt.dateFormat = "HH:mm"
         return fmt
     }()
         
     var body: some View {
         NavigationView {
             VStack {
-                Button {
-                    healthKitModel.getSleepAnalysis(fallingAsleepTime: fallingAsleepTime, wakeUpTime: wakeUpTime)
-                } label: {
-                    Text("睡眠データの取得を試みる")
-                }
-
                 List {
                     if healthKitModel.dataSource.count == 0 {
                         Text("データがありません")
                     } else {
                         ForEach( healthKitModel.dataSource ){ item in
-                            if item.sleepStatus == "0" {
-                                Text("全就寝時間")
-                            } else if item.sleepStatus == "1" {
-                                Text("睡眠時間")
-                            } else if item.sleepStatus == "2" {
-                                Text("覚醒")
-                            } else if item.sleepStatus == "3" {
-                                Text("コア")
-                            } else if item.sleepStatus == "4" {
-                                Text("深い睡眠")
-                            } else if item.sleepStatus == "5" {
-                                Text("レム")
-                            } else {
-                                Text("その他(sleepStatusが0~5以外)")
-                            }
                             HStack{
-                                Text(fmt.string(from: item.startDateTime))
-                                Text(fmt.string(from: item.endDateTime))
-                                Text("ステータス: \(item.sleepStatus)")
+                                if item.sleepStatus == "0" {
+                                    Text("全就寝時間")
+                                } else if item.sleepStatus == "1" {
+                                    Text("睡眠時間")
+                                } else if item.sleepStatus == "2" {
+                                    Text("覚醒")
+                                } else if item.sleepStatus == "3" {
+                                    Text("コア")
+                                } else if item.sleepStatus == "4" {
+                                    Text("深い睡眠")
+                                } else if item.sleepStatus == "5" {
+                                    Text("レム")
+                                } else {
+                                    Text("その他(sleepStatusが0~5以外)")
+                                }
+                                Text(fmtDate.string(from: item.startDateTime))
+                                Text(fmtTime.string(from: item.startDateTime) + " ~ " + fmtTime.string(from: item.endDateTime))
                             }
                         }
                     }
                 }
+            }.onAppear {
+                healthKitModel.getSleepAnalysis(fallingAsleepTime: fallingAsleepTime, wakeUpTime: wakeUpTime)
+            }.onDisappear {
+                healthKitModel.dataSource.removeAll()
             }
         }
     }
