726dd2e8c442d628ed0ba3f2fa5a77d6f35ecd4c
zunema <yxxa.abc@gmail.com>
2023-10-08 18:01:44 +0900

    心拍数データの処理を追加

--- HealthKitApi/View/HealthKit/HealthKitContentView.swift
+++ HealthKitApi/View/HealthKit/HealthKitContentView.swift
@@ -12,6 +12,8 @@ import HealthKit
 struct HealthKitContentView: View {
     
     @ObservedObject var healthKitModel: HealthKitModel = HealthKitModel()!
+    @ObservedObject var sleepModel: SleepModel = SleepModel()
+    @ObservedObject var heartRateModel: HeartRateModel = HeartRateModel()
     @State private var fallingAsleepTime = Date()
     @State private var wakeUpTime = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
     
@@ -20,19 +22,22 @@ struct HealthKitContentView: View {
             
             DatePicker(selection: $fallingAsleepTime) {
                 HStack {
-                    Image(systemName: "moon")
-                    Text("入眠時刻を任意で指定")
+                    Image(systemName: "clock.badge")
+                    Text("開始時刻を任意で指定")
                 }
             }
             DatePicker(selection: $wakeUpTime) {
                 HStack {
-                    Image(systemName: "sun.haze")
-                    Text("起床時刻を任意で指定")
+                    Image(systemName: "clock.badge.fill")
+                    Text("終了時刻を任意で指定")
                 }
             }
             if healthKitModel.permissionFlg {
-                NavigationLink(destination: SleepContentView(healthKitModel: healthKitModel, fallingAsleepTime: $fallingAsleepTime, wakeUpTime: $wakeUpTime)) {
+                NavigationLink(destination: SleepContentView(healthKitModel: healthKitModel, sleepModel: sleepModel, fallingAsleepTime: $fallingAsleepTime, wakeUpTime: $wakeUpTime)) {
                     Text("夢の取得情報を確認")
+                }
+                NavigationLink(destination: HeartRateContentView(healthKitModel: healthKitModel, heartRateModel: heartRateModel, fallingAsleepTime: $fallingAsleepTime, wakeUpTime: $wakeUpTime)) {
+                    Text("心拍数の取得情報を確認")
                 }
             }
 
