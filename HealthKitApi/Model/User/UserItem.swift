ec14f49232b71a78dfadea76b825142aceb51722
zunema <yxxa.abc@gmail.com>
2023-10-09 17:31:27 +0900

    ユーザIDの登録処理を作成

---
+++ HealthKitApi/Model/User/UserItem.swift
@@ -0,0 +1,14 @@
+//
+//  UserItem.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/10/09.
+//
+
+import SwiftUI
+
+struct UserItem: Identifiable {
+    var id: String
+    var userName: String
+    var uuid: String
+}
--- HealthKitApi/View/User/UserConfirmView.swift
+++ HealthKitApi/View/User/UserConfirmView.swift
@@ -13,40 +13,48 @@ import HealthKit
 
 struct UserConfirmView: View {
     
-    @State var userName = ""
-    @State var userId = ""
-    let userUuid = Auth.auth().currentUser!.uid
+    @ObservedObject var userModel: UserModel = UserModel()
+    @State var countText: String = "none"
+    @State var userCreate: Bool = false
     
     var body: some View {
         NavigationView {
             VStack {
-                Text(userUuid)
             
-                TextField(text: $userName) {
-                    Text("登録するユーザ名")
-                }
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                TextField(text: $userId) {
-                    Text("登録するユーザID")
+                TextField(text: $userModel.user.userName) {
+                    Text("登録する名前")
                 }
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
                 
                 Button {
-                    Firestore.firestore().collection("users").document(userUuid)
-                        .setData(
-                            [
-                                "userUuid": userUuid,
-                                "userID": userId,
-                                "userName": userName
-                            ]
-                        )
+                    Task {
+                        await userIdCreate()
+                        userCreate = true
+                    }
                 } label: {
-                    Text("ユーザ情報の登録")
+                    Text("ユーザIDを生成する")
                 }
-
+                
+                if userCreate {
+                    Button {
+                        userModel.save(userModel: userModel)
+                    } label: {
+                        Text("ユーザ情報の登録")
+                    }
+                } else {
+                    Text("ユーザIDを作成してください")
+                }
+                
             }
         }
+    }
+    
+    // ※ユーザIDの更新ができてしまうため、改修が必須
+    func userIdCreate() async {
+        let aggregateSnapshot = try! await Firestore.firestore().collection("users").count.getAggregation(source: .server)
+        let userId = aggregateSnapshot.count.intValue + 1
+        userModel.user.id = String(userId)
+        print("登録されるユーザID: \(userModel.user.id)")
     }
 }
