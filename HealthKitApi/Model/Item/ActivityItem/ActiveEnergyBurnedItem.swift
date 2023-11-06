5dd5a109f3d46cb017e787c96c0d80b1c6061dec
zunema <yxxa.abc@gmail.com>
2023-11-06 18:31:33 +0900

    アクティブエネルギーデータの取得処理を実装

---
+++ HealthKitApi/Model/Item/ActivityItem/ActiveEnergyBurnedItem.swift
@@ -0,0 +1,8 @@
+//
+//  ActiveEnergyBurnedItem.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/11/06.
+//
+
+import Foundation
---
+++ HealthKitApi/View/HealthKit/ActivityView/ActivityEnergyBurnedView.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityEnergyBurnedView.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/11/06.
+//
+
+import Foundation
