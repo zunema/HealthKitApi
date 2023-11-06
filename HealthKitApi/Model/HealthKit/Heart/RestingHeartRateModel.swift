766c94d18677fc739c682a2a63ddf181083ac2fd
zunema <yxxa.abc@gmail.com>
2023-11-06 17:05:30 +0900

    安静時心拍数データの取得処理を実装

---
+++ HealthKitApi/Model/HealthKit/Heart/RestingHeartRateModel.swift
@@ -0,0 +1,8 @@
+//
+//  RestingHeartRateModel.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/11/06.
+//
+
+import Foundation
---
+++ HealthKitApi/View/HealthKit/HeartView/RestingHeartRateContentView.swift
@@ -0,0 +1,8 @@
+//
+//  RestingHeartRateContentView.swift
+//  HealthKitApi
+//
+//  Created by 飯尾悠也 on 2023/11/06.
+//
+
+import Foundation
